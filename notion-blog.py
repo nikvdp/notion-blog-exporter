218c9daf64ba651cb864a698d4470583f1ee5537
Nik V <github@nikvdp.com>
2020-11-04 17:56:31 -0800

    ♻️ refactor, add collect from notion method

--- notion-blog.py
+++ notion-blog.py
@@ -22,6 +22,7 @@ from notion.block import (
 )
 from typing import List, Optional
 
+
 # TODO: maybe use puppeteer to grab this going forward?
 NOTION_TOKEN = "***REMOVED***"
 HUGO_POSTS_LOCATION = (
@@ -30,25 +31,36 @@ HUGO_POSTS_LOCATION = (
 
 client = NotionClient(token_v2=NOTION_TOKEN)
 
-# id of blog posts page: ***REMOVED***
 
-BLOG_POST_PAGE_ID = "***REMOVED***"
-# find blog posts page
-blog_posts_page = client.get_block(BLOG_POST_PAGE_ID)
+def collect_notion_posts(
+    source_block_or_page_id: str = "***REMOVED***",
+) -> List[BlogPost]:
+    """Retrieve the list of published posts from Notion
 
-published = next(
-    filter(lambda x: x.title == "Published", blog_posts_page.children), None
-)
+    Args:
+        source_block_or_page_id (str): A block or page whose children are the
+            blog posts you wish to publish.
 
-posts_to_publish = blog_posts_page.children[0].children
+    Returns:
+        List[BlogPost]: A list of blog posts
+    """
+    # id of blog posts page: ***REMOVED***
 
-sway_i3 = posts_to_publish[0]
+    # find blog posts page
+    blog_posts_page = client.get_block(source_block_or_page_id)
 
-img = sway_i3.children[0]
+    notion_posts = []
+    for post in blog_posts_page.children:
+        notion_record = post.get()
+        notion_posts.append(
+            BlogPost(
+                title=post.title,
+                body=blocks_to_markdown(post.children),
+                date=notion_record.get("created_time"),
+            )
+        )
 
-img.download_file("/tmp/Untitled.png")
-
-blog_post = posts_to_publish[1]
+    return notion_posts
 
 
 def listblock_to_markdown_handler(block):
@@ -66,19 +78,16 @@ def listblock_to_markdown_handler(block):
     return f"{output}\n"
 
 
-def blocks_to_markdown(page):
+def blocks_to_markdown(root_block):
     markdown_out = ""
-    for block in blog_post.children:
+    for block in root_block:
         markdown_out += block_to_markdown(block) or ""
 
-    with open("/tmp/out.md", "w") as fp:
-        fp.write(markdown_out)
-    print(markdown_out)
     return markdown_out
 
 
 def block_to_markdown(block):
-    default_handler = lambda block: f"\n{block.title}\n"
+    default_handler = lambda block: f"{block.title}\n"
 
     handlers = {
         # TODO: list blocks need to handle indentation for continuing lines
@@ -86,24 +95,23 @@ def block_to_markdown(block):
         + "> ".join([f"{x}\n" for x in block.title.split("\n")]),
         NumberedListBlock: listblock_to_markdown_handler,
         BulletedListBlock: listblock_to_markdown_handler,
-        HeaderBlock: lambda block: f"# {block.title}",
-        SubheaderBlock: lambda block: f"## {block.title}",
-        SubsubheaderBlock: lambda block: f"### {block.title}",
+        HeaderBlock: lambda block: f"\n# {block.title}\n",
+        SubheaderBlock: lambda block: f"\n## {block.title}\n",
+        SubsubheaderBlock: lambda block: f"\n### {block.title}\n",
         CalloutBlock: lambda block: f"> {block.icon} "
         + "> ".join([f"{x}\n" for x in block.title.split("\n")]),
         CodeBlock: lambda block: f"\n```{block.language.lower()}\n{block.title}\n```\n",
+        ImageBlock: lambda block: f"\n `<an-image-goes-here>` \n",
     }
 
     return handlers.get(type(block), default_handler)(block)
 
 
-def read_post():
-    published_posts = blog_posts_page.children[0].children
-
-
 def main():
     hugo_posts = collect_hugo_posts()
     hugo_published = [p for p in hugo_posts if not p.draft]
+
+    notion_posts = collect_notion_posts()
     # import IPython;IPython.embed()
 
     # TODO:
@@ -115,6 +123,16 @@ def main():
     # - iterate over published files in hugo
     # - if any pages in notion are not published, retrieve from notion and write as hugo
     # - then push the hugo repo
+
+
+@dataclass
+class BlogPost:
+    title: str
+    date: datetime
+    body: str
+    draft: Optional[bool] = None
+    last_edited: Optional[datetime] = None
+    metadata: Optional[any] = None
 
 
 @dataclass
